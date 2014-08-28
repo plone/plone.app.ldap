plone/plone.app.ldap

commit f0bff65afc97ed4d1dddd9ec3e4e4bcd8b9467de

Update package dependencies

setup.py
@@ -30,10 +30,17 @@ setup(name='plone.app.ldap',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # "python-ldap",
-          "Products.CMFCore",
-          "Products.PloneLDAP",
-          "setuptools"
+          'plone.memoize',
+          'Products.CMFCore',
+          'Products.GenericSetup',
+          'Products.PloneLDAP',
+          'python-ldap',
+          'setuptool',
+          'zope.component',
+          'zope.i18nmessageid',
+          'zope.interface',
+          'zope.lifecycleevent',
+          'zope.schema',
       ],
       entry_points="""
       # -*- Entry points: -*-

commit 53940eb5e226465dc264e680246b113f2b553f0e

fix setuptools (not 'setuptool')
Was broken in f0bff65, leading to this error:

Develop distribution: plone.app.ldap 1.3.2.dev0
uses namespace packages but the distribution does not require setuptools.
Couldn't find index page for 'setuptool' (maybe misspelled?)
Getting distribution for 'setuptool'.
Couldn't find index page for 'setuptool' (maybe misspelled?)
While:
  Updating client1.
  Getting distribution for 'setuptool'.
Error: Couldn't find a distribution for 'setuptool'.

setup.py
@@ -35,7 +35,7 @@ setup(name='plone.app.ldap',
           'Products.GenericSetup',
           'Products.PloneLDAP',
           'python-ldap',
-          'setuptool',
+          'setuptools',
           'zope.component',
           'zope.i18nmessageid',
           'zope.interface',