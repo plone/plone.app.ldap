plone/plone.app.ldap

commit 1d0101bfd6bdf332bfc4c258a398da9a96bd36c8

store blank schema property fields as empty strings, not None
An empty optional form field is returned as None, but this was being stored in
the LDAPUserFolder object (and also the export from portal_setup) as the
literal string 'None'.

This change makes plone.app.ldap match the behavior of the ZMI (LDAPUserFolder).

plone/app/ldap/engine/schema.py
@@ -16,3 +16,11 @@ class LDAPProperty(SimpleItem):
         self.plone_name=plone_name
         self.multi_valued=multi_valued
         self.binary=binary
+
+    def __setattr__(self, name, value):
+        if name in ('ldap_name', 'plone_name', 'description'):
+            # Store blank fields as empty strings to avoid exporting a
+            # literal 'None' with e.g. str(property.attr).
+            self.__dict__[name] = value or u""
+        else:
+            self.__dict__[name] = value

commit f31d714356513cb26f3a169861abe7f263171e05

fix __setattr__ for new-style classes
Unbreak product installation (broken in 1d0101b).

plone/app/ldap/engine/schema.py
@@ -21,6 +21,5 @@ class LDAPProperty(SimpleItem):
         if name in ('ldap_name', 'plone_name', 'description'):
             # Store blank fields as empty strings to avoid exporting a
             # literal 'None' with e.g. str(property.attr).
-            self.__dict__[name] = value or u""
-        else:
-            self.__dict__[name] = value
+            value = value or u""
+        super(LDAPProperty, self).__setattr__(name, value)